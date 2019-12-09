7799d97593182f83c29dea56485d1c8cdcf68483
Kasun Gamage <kasun@datacaptureexperts.com.au>
2019-12-09 11:15:56 +0530

    initial commit

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/CertificateSender")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            getIndividualSearchList();
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public string getIndividualSearchList()
+        {
+            string BaseUrl = "https://sandbox.digitalhealth.gov.au/FhirServerR4-PDX";
+            string cerPath = "C:\\Users\\KasunGamage\\source\\repos\\WebApplication1\\WebApplication1\\Files\\public.crt.pem";
+            string cerKey = "C:\\Users\\KasunGamage\\source\\repos\\WebApplication1\\WebApplication1\\Files\\private.key.pem";
+            string result = "0";
+
+            // A PEM file must consist of a private key, A PEM encoded file includes Base64 data.
+
+            byte[] certBuffer = GetBytesFromPEM(cerPath, "CERTIFICATE");
+            byte[] keyBuffer = GetBytesFromPEM(cerKey, "PRIVATEKEY");
+
+            X509Certificate2 certificate = new X509Certificate2(certBuffer);
+
+            RSACryptoServiceProvider prov = DecodeRSAPrivateKey(keyBuffer);
+            certificate.PrivateKey = prov;
+
+            string requestUri = BaseUrl + "/fhir/PractitionerRole?active=true&practitioner.family=Kidman&_include=PractitionerRole:practitioner&_include=PractitionerRole:location&_include=PractitionerRole:organization&_include=PractitionerRole:endpoint&_include=PractitionerRole:service";
+
+            HttpWebRequest tRequest = (HttpWebRequest)WebRequest.Create(requestUri);
+
+            tRequest.ClientCertificates.Add(certificate);
+            tRequest.Method = "GET";
+            //tRequest.ContentType = "application/json";
+
+            HttpWebResponse tResponse2 = (HttpWebResponse)tRequest.GetResponse();
+
+            using (HttpWebResponse tResponse = (HttpWebResponse)tRequest.GetResponse())
+            {
+                using (Stream dataStreamResponse = tResponse.GetResponseStream())
+                {
+                    using (StreamReader tReader = new StreamReader(dataStreamResponse))
+                    {
+                        var sResponseFromServer = tReader.ReadToEnd();
+                        var str = sResponseFromServer;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        byte[] GetBytesFromPEM(string pemString, string section)
+        {
+            var header = String.Format("-----BEGIN {0}-----", section);
+            var footer = String.Format("-----END {0}-----", section);
+
+            var start = pemString.IndexOf(header, StringComparison.Ordinal);
+            if (start < 0)
+                return null;
+
+            start += header.Length;
+            var end = pemString.IndexOf(footer, start, StringComparison.Ordinal) - start;
+
+            if (end < 0)
+                return null;
+
+            return Convert.FromBase64String(pemString.Substring(start, end));
+        }
+       
+        static bool verbose = false;
+
+        public RSACryptoServiceProvider DecodeRSAPrivateKey(byte[] privkey)
+        {
+            byte[] MODULUS, E, D, P, Q, DP, DQ, IQ;
+
+            // --------- Set up stream to decode the asn.1 encoded RSA private key ------
+            MemoryStream mem = new MemoryStream(privkey);
+            BinaryReader binr = new BinaryReader(mem);  //wrap Memory Stream with BinaryReader for easy reading
+            byte bt = 0;
+            ushort twobytes = 0;
+            int elems = 0;
+            try
+            {
+                twobytes = binr.ReadUInt16();
+                if (twobytes == 0x8130) //data read as little endian order (actual data order for Sequence is 30 81)
+                    binr.ReadByte();    //advance 1 byte
+                else if (twobytes == 0x8230)
+                    binr.ReadInt16();    //advance 2 bytes
+                else
+                    return null;
+
+                twobytes = binr.ReadUInt16();
+                if (twobytes != 0x0102) //version number
+                    return null;
+                bt = binr.ReadByte();
+                if (bt != 0x00)
+                    return null;
+
+
+                //------ all private key components are Integer sequences ----
+                elems = GetIntegerSize(binr);
+                MODULUS = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                E = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                D = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                P = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                Q = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                DP = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                DQ = binr.ReadBytes(elems);
+
+                elems = GetIntegerSize(binr);
+                IQ = binr.ReadBytes(elems);
+
+                Console.WriteLine("showing components ..");
+                if (verbose)
+                {
+                    showBytes("\nModulus", MODULUS);
+                    showBytes("\nExponent", E);
+                    showBytes("\nD", D);
+                    showBytes("\nP", P);
+                    showBytes("\nQ", Q);
+                    showBytes("\nDP", DP);
+                    showBytes("\nDQ", DQ);
+                    showBytes("\nIQ", IQ);
+                }
+
+                // ------- create RSACryptoServiceProvider instance and initialize with public key -----
+                CspParameters CspParameters = new CspParameters();
+                CspParameters.Flags = CspProviderFlags.UseMachineKeyStore;
+                RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(1024, CspParameters);
+                RSAParameters RSAparams = new RSAParameters();
+
+                RSAparams.Modulus = MODULUS;
+                RSAparams.Exponent = E;
+                RSAparams.D = D;
+                RSAparams.P = P;
+                RSAparams.Q = Q;
+                RSAparams.DP = DP;
+                RSAparams.DQ = DQ;
+                RSAparams.InverseQ = IQ;
+                RSA.ImportParameters(RSAparams);
+                return RSA;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                binr.Close();
+            }
+        }
+
+        private static int GetIntegerSize(BinaryReader binr)
+        {
+            byte bt = 0;
+            byte lowbyte = 0x00;
+            byte highbyte = 0x00;
+            int count = 0;
+            bt = binr.ReadByte();
+            if (bt != 0x02)     //expect integer
+                return 0;
+            bt = binr.ReadByte();
+
+            if (bt == 0x81)
+                count = binr.ReadByte();    // data size in next byte
+            else
+              if (bt == 0x82)
+            {
+                highbyte = binr.ReadByte(); // data size in next 2 bytes
+                lowbyte = binr.ReadByte();
+                byte[] modint = { lowbyte, highbyte, 0x00, 0x00 };
+                count = BitConverter.ToInt32(modint, 0);
+            }
+            else
+            {
+                count = bt;     // we already have the data size
+            }
+
+            while (binr.ReadByte() == 0x00)
+            {   //remove high order zeros in data
+                count -= 1;
+            }
+            binr.BaseStream.Seek(-1, SeekOrigin.Current);       //last ReadByte wasn't a removed zero, so back up a byte
+            return count;
+        }
+
+        private static void showBytes(String info, byte[] data)
+        {
+            Console.WriteLine("{0} [{1} bytes]", info, data.Length);
+            for (int i = 1; i <= data.Length; i++)
+            {
+                Console.Write("{0:X2} ", data[i - 1]);
+                if (i % 16 == 0)
+                    Console.WriteLine();
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
